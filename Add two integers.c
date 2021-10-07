60e19ad2ff10f12c94b32a657c14d1517063b257
ankit0551 <92089461+ankit0551@users.noreply.github.com>
2021-10-07 17:30:46 +0530

    Add files via upload

---
+++ Add two integers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	int a,b,sum;
+	printf("\nThis program adds two numbers.");
+	printf("\nEnter two numbers in the form: nnn nnn <return>\n");
+	scanf("%d%d", &a, &b);
+	
+	sum=a+b;
+	
+	 printf("the total is: %d\n\n", sum);
+	 	
+}
