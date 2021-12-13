b1c3064dc5737b27c7d8a622f93099f0b2a59174
ankit0551 <92089461+ankit0551@users.noreply.github.com>
2021-12-13 23:38:17 +0530

    Add files via upload

---
+++ sum_of_n_even_natural_number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,sum=0;
+    printf("Enter the last term of natural number: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(i%2==0)
+        sum+=i;
+    }
+    printf("Sum = %d",sum);
+    return 0;
+}
