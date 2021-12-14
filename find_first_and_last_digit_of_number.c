7a3acc9ad41f1c364adafcf920a714a5d2865c8e
ankit0551 <92089461+ankit0551@users.noreply.github.com>
2021-12-14 23:38:57 +0530

    Add files via upload

---
+++ find_first_and_last_digit_of_number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,first_digit,last_digit;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    last_digit=n%10;
+    while(n>=10)
+    {
+        n/=10;
+    }
+    printf("first digit = %d\nlast digit = %d",n,last_digit);
+    return 0;
+
+}
